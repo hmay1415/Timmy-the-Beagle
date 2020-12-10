97509a878fafdcaa6be07353e35d755ba85a051a
Hilary May <hilaryfmay@gmail.com>
2020-12-10 18:00:46 -0500

    backing up Tim site

---
+++ name.js
@@ -0,0 +1,7 @@
+var userName = prompt('Please enter your name for a fun surprise');
+
+if (userName == 'Hilary'){
+    document.write('A friend of my Mom ' + userName)
+} else {
+document.write('A Friend of ' + userName);
+}
